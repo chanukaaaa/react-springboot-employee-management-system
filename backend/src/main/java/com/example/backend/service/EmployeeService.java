65b255a8dc5b29198a05832b91d3d0e0b53b02c1
Chanuka <it21281082@my.sliit.lk>
2024-06-28 19:31:56 +0530

    Added files to the Repository

---
+++ backend/src/main/java/com/example/backend/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.backend.service;
+
+import com.example.backend.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+}
---
+++ backend/src/main/java/com/example/backend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.backend.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.backend.dto.EmployeeDto;
+import com.example.backend.entity.Employee;
+import com.example.backend.mapper.EmployeeMapper;
+import com.example.backend.repository.EmployeeRepository;
+import com.example.backend.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeRepository employeeRepository;
+
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto)
+
+        return null;
+    }
+
+}
